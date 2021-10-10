5e00ed164602c221549cea598d00cd7fc3a99279
koresachin7 <koresachin7@gmail.com>
2021-10-10 23:07:20 +0530

    Added : - To Enter Valid First Name

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
+/**
+ * Purpose - To validate the first name using regex
+ * @author - Sachin Kore
+ * @version - 16.0
+ */
 
 public class RegexMain {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration ");
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.validateUserDetails();
     }
 }
---
+++ src/com/bridgelabz/UserDetails.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+/**
+ * Purpose - Implementing getters and setters
+ */
+public class UserDetails {
+    String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
